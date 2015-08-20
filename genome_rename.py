441bbcb4685b1e75fdd749731ef3285348308718
alvaralmstedt <Vnetbio3>
2015-08-20 11:15:26 +0200

    added non-zipped file mode to rename

--- genome_rename.py
+++ genome_rename.py
@@ -8,24 +8,33 @@ import datetime
 parser = argparse.ArgumentParser()
 
 parser.add_argument("folder", nargs="?", type=str, help='specify folder path, else cwd')
-
+parser.add_argument("-z", "--zip", action="store_true", help='specifies zipped files')
 
 args = parser.parse_args()
 
 user_directory = args.folder
+zipped = args.zip
 
-for subdir in os.listdir(user_directory):
-    if os.path.isdir(str(user_directory + "/" + subdir)):
-        os.chdir(str(user_directory) + "/" + str(subdir))
+if zipped:
+    for subdir in os.listdir(user_directory):
+        if os.path.isdir(str(user_directory + "/" + subdir)):
+            os.chdir(str(user_directory) + "/" + str(subdir))
+            for filnam in os.listdir("."):
+                if filnam.endswith(".tgz"):
+                    tfile = tarfile.open(filnam)
+                    tfile.extractall(".")
         for filnam in os.listdir("."):
-            if filnam.endswith(".tgz"):
-            	tfile = tarfile.open(filnam)
-            	tfile.extractall(".")
-	for filnam in os.listdir("."):
-		os.rename(filnam, subdir + filnam)
-        os.chdir("..")
-    else:
-        print "%s is a file, continuing..." % subdir
+            os.rename(filnam, subdir + filnam)
+            os.chdir("..")
+        else:
+            print "%s is a file, continuing..." % subdir
+else:
+    for subdir in os.listdir(user_directory):
+        if os.path.isdir(str(user_directory + "/" + subdir)):
+            os.chdir(str(user_directory) + "/" + str(subdir))
+            for filnam in os.listdir("."):
+                if not filnam.endswith(".tgz"):
+                    os.rename(filnam, subdir + filnam)
 
 print "done at %s" % datetime.datetime.now()
 
