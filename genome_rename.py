e8d6a14e8abe49c36d83a401e20014a52dfd95d4
alvaralmstedt <Vnetbio3>
2015-08-25 15:32:34 +0200

    fix

--- genome_rename.py
+++ genome_rename.py
@@ -18,10 +18,11 @@ def concatenate(f_end, folder, namae):
         print str(f_end)
         with open("concat_" + str(folder) + str(namae), 'w') as outfile:
             for i in folder:
+                print "before if"
                 if i.endswith(str(f_end)) and not i.startswith("concat_"):
-                    print "line 22"
+                    print "line 23"
                     with open(str(i)) as infile:
-                        print "line 24, opened"
+                        print "line 25, opened"
                         for line in infile.readlines():
                             outfile.write(line)
                             print line
