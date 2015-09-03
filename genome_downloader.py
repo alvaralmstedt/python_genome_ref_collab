5121cfa83012a9d9f313c02f4a50d07ba9b2a45a
alvaralmstedt <Vnetbio3>
2015-09-03 16:07:48 +0200

    Testing ASSEMBLY_BACTERIA implementation

--- genome_downloader.py
+++ genome_downloader.py
@@ -334,7 +334,7 @@ for key in genome_subfolders.keys():
                         print "elif passed"
 #                        for file_id in fil:
 #                        print file_id
-                        print "Downloading %s from %s via urrlib at %s" % (fil, pwd + "/" + str(key) + "/" + str(annoying_folders[key]) + str(annoying_folders[key[genome_subfolders[key]]]), datetime.datetime.now())
+                        print "Downloading %s from %s via urrlib at %s" % (fil, pwd + "/" + str(key) + "/" + str(annoying_folders[key]) + str(annoying_folders[key[0]]), datetime.datetime.now())
                         urllib.urlretrieve("ftp://ftp.wip.ncbi.nlm.nih.gov" + "/" + str(pwd) + "/" + str(key) + "/" + str(annoying_folders[key]) + "/" + str(genome_subfolders[key[genome_subfolders.index(key)]]), out + str(key) + "/" + str(fil) + str(file_id))
                         print "%s was downloaded to the folder %s at time: %s" % (fil, key, datetime.datetime.now())
                 except Exception:
