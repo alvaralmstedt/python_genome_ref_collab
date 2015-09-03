8de0a9736b7b9bd84f4a503b5314b56104f3184d
alvaralmstedt <Vnetbio3>
2015-09-03 14:41:45 +0200

    adding support for ASSEMBLY_BACTERIA. testing.

--- genome_downloader.py
+++ genome_downloader.py
@@ -332,11 +332,11 @@ for key in genome_subfolders.keys():
                         print "%s was downloaded to the folder %s at time: %s" % (fil, key, datetime.datetime.now())
                     elif "ASSEMBLY_BACTERIA" in pwd:
                         print "elif passed"
-                        for file_id in fil:
-                            print file_id
-                            print "Downloading %s from %s via urrlib at %s" % (fil, pwd + "/" + str(key) + "/" + str(annoying_folders[key]) + "/" + str(genome_subfolders[key]), datetime.datetime.now())
-                            urllib.urlretrieve("ftp://ftp.wip.ncbi.nlm.nih.gov" + "/" + str(pwd) + "/" + str(key) + "/" + str(annoying_folders[key]) + "/" + str(key[fil[file_id]]), out + str(key) + "/" + str(fil) + str(file_id))
-                            print "%s was downloaded to the folder %s at time: %s" % (fil, key, datetime.datetime.now())
+#                        for file_id in fil:
+#                        print file_id
+                        print "Downloading %s from %s via urrlib at %s" % (fil, pwd + "/" + str(key) + "/" + str(annoying_folders[key]) + "/" + str(genome_subfolders[key]), datetime.datetime.now())
+                        urllib.urlretrieve("ftp://ftp.wip.ncbi.nlm.nih.gov" + "/" + str(pwd) + "/" + str(key) + "/" + str(annoying_folders[key]) + "/" + str(genome_subfolders[key]), out + str(key) + "/" + str(fil) + str(file_id))
+                        print "%s was downloaded to the folder %s at time: %s" % (fil, key, datetime.datetime.now())
                 except Exception:
                     print "%s couldn't be downloaded at time %s" % (fil, datetime.datetime.now())
                     # print something to error file here
