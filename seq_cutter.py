7c18126a4bc2fec8906933046dbd8a02235bfd78
alvaralmstedt <Vnetbio3>
2015-10-05 11:08:06 +0200

    added seq_cutter, a tool for assisting with alignment concatenation. Does not yet work.

--- seq_cutter.py
+++ seq_cutter.py
@@ -16,14 +16,15 @@ function_to_be_used = str(args.fun)
 comppath = str(args.completedpath)
 tabbedpath = str(args.tabpath)
 species = str(args.species)
+# cwd = os.getcwd() + "/"
 
-# Initialises files with the correct species filename
+# Initialises (creates) files with the correct species filename
 def toucher(inpath, outpath, spec):
     infiles = os.listdir(inpath)
     for each_file in infiles:
-        with open(each_file) as infile:
-            for lines in infile.readlines:
-                if lines.startswith(species) and not os.path.isfile(outpath + "/" + spec + ".aln"):
+        with open(inpath + "/" + each_file) as infile:
+            for line in infile.readlines():
+                if line.startswith(species) and not os.path.isfile(outpath + "/" + spec + ".aln"):
                     subprocess.call(['touch', outpath + "/" + spec + ".aln"])
 
 # Inserts header from the correct species into output file
@@ -32,12 +33,13 @@ def headers(sourcepath, destpath):
     outfiles = os.listdir(destpath)
     for namedfiles in outfiles:
         for each_file in infiles:
-            with open(each_file) as infile:
-                for lines in infile.readlines():
-                    lines = lines.split("   ")
-                    if lines.startswith(str(namedfiles)):
-                        with open(namedfiles, "a") as destination:
-                            destination.write(str(lines))
+            with open(sourcepath + "/" + each_file) as infile:
+                for line in infile:
+                    splitlines = line.split("   ")
+                    for word in splitlines:
+                        if word.startswith(str(namedfiles)):
+                            with open(namedfiles, "a") as destination:
+                                destination.write(str(line))
 
 # Inserts the sequences from the correct species into the correct files
 def sequences(sourcepath, destpath):
@@ -45,7 +47,7 @@ def sequences(sourcepath, destpath):
     outfiles = os.listdir(destpath)
     for namedfiles in outfiles:
         for each_file in infiles:
-            with open(each_file) as infile:
+            with open(sourcepath + "/" + each_file) as infile:
                 for lines in infile.readlines():
                     if lines.startswith(namedfiles):
                         with open(namedfiles, "a") as destination:
@@ -53,10 +55,13 @@ def sequences(sourcepath, destpath):
 
 
 if function_to_be_used == "t":
+    print "You have selected 'toucher' with paths %s, %s and species %s" % (tabbedpath, comppath, species)
     toucher(tabbedpath, comppath, species)
 elif function_to_be_used == "h":
+    print "You have selected 'headers' with paths %s and %s" % (tabbedpath, comppath)
     headers(tabbedpath, comppath)
 elif function_to_be_used == "s":
+    print "You have selected 'sequences' with the paths %s and %s" % (tabbedpath, comppath)
     sequences(tabbedpath, comppath)
 else:
     print "Please input which function to use: t = toucher; h = headers; s = sequences"
